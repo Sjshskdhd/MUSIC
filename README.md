2838a0d1563b02c552732a6602a25bfe67d452a3
THUNDER <103184104+MH7MD1@users.noreply.github.com>
2022-06-27 13:13:39 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+ ### 🅰︎sᴜᴋᴀ 🅼︎ᴜsɪᴄ 🅱︎ᴏᴛ
+
+### ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ ᴘʀᴇᴍɪᴜᴍ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ sᴜᴍɪᴛ ʏᴀᴅᴀᴠ.
+
+<p align="center"><a href="https://t.me/World_FriendShip_Zone"><img src="https://te.legra.ph/file/3752041b671e0afc6ada2.jpg"></a></p>
+
+
+ <h3 align="center">
+    ─ ᴅᴇᴩʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ ─
+</h3>
+
+<p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/Sumit9969/AsukaMusicBot"> <img src="https://img.shields.io/badge/Deploy%20On%20Heroku-black?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+
+
+### sᴛʀɪɴɢ sᴇssɪᴏɴ
+
+[![GenerateString](https://img.shields.io/badge/repl.it-generateString-black)](https://t.me/Hana_Session_Bot)
+
+
+### ꜱᴜᴘᴘᴏʀᴛ ᴀɴᴅ ᴜᴘᴅᴀᴛᴇꜱ
+<a href="https://t.me/TechQuardSupport"><img src="https://img.shields.io/badge/Join-Group%20Support-black.svg?style=for-the-badge&logo=Telegram"></a> <a href="https://t.me/TechQuard"><img src="https://img.shields.io/badge/Join-Updates%20Channel-black.svg?style=for-the-badge&logo=Telegram"></a>
+
+ 
---
+++ plugins/alive.py
@@ -0,0 +1,95 @@
+import asyncio
+from time import time
+from datetime import datetime
+from modules.config import BOT_USERNAME
+from modules.helpers.filters import command
+from modules.helpers.command import commandpro
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/3752041b671e0afc6ada2.jpg",
+        caption=f"""**━━━━━━━━━━━━━━━━━
+✨ انا بوت 
+تشغيل الموسيقي في المحادثات...
+للاستفسار راسل المطور
+مطوري [ELLIOT](https://t.me/T_8_T_T)...
+━━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+            InlineKeyboardButton("📝 الاومر", url="https://te.legra.ph/%F0%9D%99%B2%E1%B4%8F%E1%B4%8D%E1%B4%8D%E1%B4%80%C9%B4%E1%B4%85s-06-26"),
+            InlineKeyboardButton("⚕️ المطور", url="https://t.me/T_8_T_T")
+        ],
+        [
+            InlineKeyboardButton("👥 جروب الدعم", url="https://t.me/CHVOTLX"),
+            InlineKeyboardButton("📣 قناة السورس", url="https://t.me/T_8_T_T")
+        ],
+        [
+            InlineKeyboardButton("💠 ايثون 💠", url="https://t.me/EITHON1")
+        ]
+   
+     ]
+  ),
+)
+    
+    
+@Client.on_message(commandpro(["/start", "/alive", "Sumit", "تنصيب"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/c264771ae8331ab9a7260.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+             [
+            [
+                InlineKeyboardButton(text="👥 ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/CHVOTLX"),
+                InlineKeyboardButton(text="📣 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/T_8_T_T"),
+            ]
+        ]
+     ),
+  ) 
+
+
+@Client.on_message(commandpro(["/updates", "Channel", "/openbaby"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/e866cd93108978ef6faef.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴊᴏɪɴ ᴛʜᴇsᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/EITHON1")
+                ]
+            ]
+        ),
+    )
